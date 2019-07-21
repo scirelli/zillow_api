fb0a8eb65c6018cd9b09b142d2c11ce811bafd2a
Ubuntu <ccirelli2@DSVM1.ysmgyhy02j5uzmarbfmuxylp0d.bx.internal.cloudapp.net>
2019-07-21 19:41:31 +0000

    -q

--- code/script1_zillow_api.py
+++ code/script1_zillow_api.py
@@ -1,4 +1,3 @@
-
 # IMPORT LIBRARIES
 import pyzillow
 from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults, GetUpdatedPropertyDetails 
@@ -8,35 +7,23 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import urllib
+import os
 
 
-
-# IMPORT MODULES
-import module1_zillow_api as m1
-
+# IMPORT MODULES----------------------------------------------------------------
+#import module1_zillow_api as m1
 
 
-user_name       = 'ccirelli2'
-pswd            = 'Work4starr'
+# ZILLOW API USER INFO----------------------------------------------------------
+user_name       = input('Input user name => ')
+pswd            = input('Input password  => ')
 web_service_id  = 'X1-ZWz1h90xzgg45n_2kpit'
 documentation   = 'https://pypi.org/project/pyzillow/'
 d2				= 'https://anchetawern.github.io/blog/2014/03/20/getting-started-with-zillow-api/'
 zillow_data = ZillowWrapper(web_service_id)
 
 
-# Test Data
-def test_location_data(return_value):
-	if returnvalue == 'address':
-		return '130 Roswell Commons Way, Roswell, GA 30076'
-	elif return_value == 'zipcode':
-		return '30076'
-
-
-# Get Zpids for a specific State / City
-list_zpids = m1.get_zpids('Roswell', 'GA')
-
-
-# Get Deep Search Results
+# GET DEEP SEARCH RESULTS-------------------------------------------------------
 def address_search(address, zipcode, output):
 	deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
 	result = GetDeepSearchResults(deep_search_response)
@@ -57,7 +44,7 @@ def address_search(address, zipcode, output):
 		zillow_id = result.zillow_id
 		return zillow_id
 
-# Get Updated Property Details
+# GET UPDATED PROPERTY DETAILS------------------------------------------------
 def zillow_id_search(zillow_id):
 	updated_property_details_response = zillow_data.\
                                     get_updated_property_details(zillow_id)
@@ -70,21 +57,14 @@ def zillow_id_search(zillow_id):
 	return None
 
 
-
-def run_zillow_id_search_4_list(list_zpids):
-
-	for zpid in list_zpids:
-		zpid_str = str(zpid)
-		try:
-			zillow_id_search(zpid_str)
-		except pyzillow.pyzillowerrors.ZillowError as err:
-			print(err)
+# TEST API ON ADDRESSES------------------------------------------------------
+os.chdir('/home/ccirelli2/Desktop/repositories/zillow_api/output')
+df_home_data = pd.read_excel('zillow_scraper_output_2019-07-21.xlsx')
+print(df_home_data.head())
 
 
 
-address_search('100 Lakeview Rdg', 30076, 'sdout')
-zillow_id_search(65448088)
-#print(list_zpids)
+
 
 
 
